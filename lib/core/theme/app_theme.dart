3ba735c64574540ff8c239d8fbb0ccc8110ab06f
tyyyrvid-a11y <tyyyrvid-a11y@users.noreply.github.com>
2026-07-11 18:29:26 -0300

    Fix Windows build compilation errors

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -55,12 +55,6 @@ class AppTheme {
       ),
       splashFactory: NoSplash.splashFactory,
       highlightColor: Colors.transparent,
-      pageTransitionsTheme: const PageTransitionsTheme(
-        builders: {
-          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-          TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
-        },
-      ),
     );
   }
 }
